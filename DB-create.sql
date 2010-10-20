98f1548d364a84e21e4491dd122b4386eac0c971
Ben Kulbertis <ben@kulbertis.org>
2010-10-19 22:34:12 -0400

    Removed old SQL file

--- DB-create.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `DB_NAME`.`tokens` (
-`id`                  BIGINT(22) UNSIGNED AUTO_INCREMENT,
-`token`               VARCHAR(32),
-`timestamp`           TIMESTAMP DEFAULT NOW(),
-PRIMARY KEY (id)
-)
